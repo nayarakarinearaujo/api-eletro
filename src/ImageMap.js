66e802be65a352771e51e441dbade5f954c38264
Nayara Karine <149000384+nayarakarinearaujo@users.noreply.github.com>
2024-09-18 13:13:30 -0300

    Ajuste na imagem de fundo do header

--- src/ImageMap.js
+++ src/ImageMap.js
@@ -1,9 +1,23 @@
-import note2 from './image/notebook.png';  
+import note2 from './image/notebook1.png';  
 import smartphone2 from './image/smarphone2.png'; 
+import camera from './image/camera.png'
+import smartwatch from './image/smartwatch.jpg'
+import notebook2 from './image/note4.png'
+import tablet from './image/tablet.png'
+import smartphone from './image/smarphone.png'
+import smartspeaker from './image/smartspeaker.png'
+import notebook from './image/notebook3.png'
 
 const ImageMap = {
   "notebook-3": note2,
   "smartphone-2": smartphone2,
+  "camera": camera,
+  "smartwatch": smartwatch,
+  "notebook-2": notebook2,
+  "tablet": tablet,
+  "smartphone": smartphone,
+  "smartspeaker": smartspeaker,
+  "notebook": notebook,
 };
 
 export default ImageMap;
